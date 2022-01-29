f1d3f74f26f7258d228387f9b1d1ccc6357ba41e
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-29 19:18:07 +0000

    create accordion and search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-function App() {
+const App = () => {
+  const items = [
+    { title: "What is Raect?", content: "Raect is a fronend js framework" },
+    { title: "Why use react?", content: "Raect is great" },
+    {
+      title: "How do you use React?",
+      content: "You use Raet by creating components",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search />
     </div>
   );
-}
-
+};
 export default App;
