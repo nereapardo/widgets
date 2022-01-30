4998a6cac7d5b838f2f30a2882b77e685b0e194d
nerea pardo <nerea.pardo.cundins@gmail.com>
2022-01-30 12:31:05 +0000

    create dropdown

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+
+const items = [
+  { title: "What is Raect?", content: "Raect is a fronend js framework" },
+  { title: "Why use react?", content: "Raect is great" },
+  {
+    title: "How do you use React?",
+    content: "You use Raet by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+  {
+    label: "The color blue",
+    value: "blue",
+  },
+];
 
 const App = () => {
-  const items = [
-    { title: "What is Raect?", content: "Raect is a fronend js framework" },
-    { title: "Why use react?", content: "Raect is great" },
-    {
-      title: "How do you use React?",
-      content: "You use Raet by creating components",
-    },
-  ];
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
